1f2e2b88f848f174104146edaf8195f275323b88
Satashee Sharma <satasheesharma@gmail.com>
2024-10-06 02:05:59 +0530

    third commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let voiceSelect=document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged= () =>{
     voices = window.speechSynthesis.getVoices();
-}
+    speech.voices=voices[0];
+    voices.forEach((voice,i) => (voiceSelect.options[i]) = new Option(voice.name,i))
+};
 
 
 
